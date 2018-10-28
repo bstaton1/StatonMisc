fa883ba4855709901c8cca8b0395bfd76e5085c3
Ben <bas0041@auburn.edu>
2018-10-28 08:45:01 -0500

    add cv2sig and sig2cv

---
+++ R/cv2sig.R
@@ -0,0 +1,18 @@
+#' Convert CV to Lognormal SD
+#'
+#' This function is used to convert the coefficient of variation of a
+#' random variable to the lognormal standard deviation, using a simple transformation
+#'
+#' @param cv a numeric vector, possibly length > 1, containing the CV to convert
+#'
+#' @return a numeric vector with the same length as \code{cv},
+#'   the lognormal standard deviation
+#'
+#' @seealso \code{\link{sig2cv}}
+#'
+#' @examples
+#' cv2sig(cv = 0.2)
+
+cv2sig = function(cv) {
+  sqrt(log((cv^2) + 1))
+}
---
+++ R/sig2cv.R
@@ -0,0 +1,18 @@
+#' Convert Lognormal SD to CV
+#'
+#' This function is used to convert the lognormal standard deviation
+#' of a random variable to the coefficient of variation, using a simple transformation
+#'
+#' @param cv a numeric vector, possibly length > 1, containing the lognormal SD to convert
+#'
+#' @return a numeric vector with the same length as \code{cv},
+#'   the coefficient of variation
+#'
+#' @seealso \code{\link{cv2sig}}
+#'
+#' @examples
+#' sig2cv(sig = 0.2)
+
+sig2cv = function(sig) {
+  sqrt(exp(sig^2) - 1)
+}
