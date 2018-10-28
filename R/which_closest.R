a98121957de20562f32dc5c0269f3a8aa13b85e0
Ben <bas0041@auburn.edu>
2018-10-28 08:33:09 -0500

    Initial commit
    Approximately 10 minutes of work prior to starting this repository. Not including the fact I've been using these functions for about 5 years without turning them into a package...shame on me

---
+++ R/which_closest.R
@@ -0,0 +1,18 @@
+#' Find index of element closest to a number
+#'
+#' This function is used to find index of the element of a vector \code{x} with the value
+#' closest to \code{y}, regardless of if it is greater or less than \code{y}
+#'
+#' @param x a numeric vector with length > 1 from which to find an element index
+#' @param y a numeric vector with length == 1, the "close to" value
+#'
+#' @return a numeric vector with length 1, which represents the index of \code{x}
+#'   that is closest to the value \code{y}
+#'
+#' @examples
+#' which_closest(x = 5:10, y = 7)
+
+which_closest = function(x, y) {
+  which.min(abs(x - y))
+}
+
