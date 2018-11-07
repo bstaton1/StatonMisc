6247367c6d5c8a8c0876ab48c7fefa60d78c2b6f
Ben <bas0041@auburn.edu>
2018-11-06 22:58:32 -0600

    add capitalize and ext_device functions

---
+++ R/capitalize.R
@@ -0,0 +1,21 @@
+#' Capitalize the first letter in a string
+#'
+#' Like \code{toupper}, but only for the first letter of a string.
+#'
+#' @param x a character vector of any length
+#'
+#' @examples
+#' capitalize("ben")
+#'
+#' @export
+
+capitalize = function(x) {
+  # check for appropriate class
+  if (!is.character(x)) stop("x must be a character")
+  # extract the first letter
+  first = substr(x, 1, 1)
+  #extract the last bit
+  last = substr(x, 2, nchar(x))
+  # capitalize the first and paste on last bit
+  paste(toupper(first), last, sep = "")
+}
---
+++ R/ext_device.R
@@ -0,0 +1,28 @@
+#' Create a new external plotting device
+#'
+#' This is a platform-flexible function for creating a new plotting device.
+#' It functions just like \code{windows()} on Windows OS, \code{quartz()} on Mac OS,
+#' and \code{x11()} on Linux OS.
+#'
+#' @param h the height, in inches
+#' @param w the width, in inches
+#' @param title the desired title of the plotting window
+#'
+#' @export
+
+ext_device = function(h = 7, w = 7, title = NULL) {
+  # determine the operating system
+  if (.Platform$OS.type == "windows") {
+    os = "win"
+  } else if (Sys.info()["sysname"] == "Darwin") {
+    os = "mac"
+  } else if (.Platform$OS.type == "unix") {
+    os = "unix"
+  } else {
+    stop("Unknown OS")
+  }
+
+  if (os == "win") windows(h = h, w = w, title = title)
+  if (os == "mac") quartz(h = h, w = w, title = title)
+  if (os == "unix") x11(h = h, w = w, title = title)
+}
