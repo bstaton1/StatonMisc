39581f7dde1cf7811dbf505777440ea31ce854d1
Ben <bas0041@auburn.edu>
2018-11-08 08:49:42 -0600

    Added more package metadata

---
+++ R/StatonMisc.R
@@ -0,0 +1,11 @@
+#'StatonMisc: Common Functions Used by B.A. Staton
+#'
+#'This package contains a somewhat random assortment of functions that are
+#'commonly used in code written by Ben Staton, including his other packages and
+#'non-public code. It includes mostly utility-type functions for tasks like
+#'transforming dates (from MM/DD/YYYY to DOY and back) and basic data
+#'transformations (\code{cv2sig}, \code{sig2cv}, \code{logis}, \code{expit}, etc.).
+#'
+#'@docType package
+#'@name StatonMisc
+NULL
