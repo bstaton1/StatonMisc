df73ed65880612ca3c859b65c6cb80b30d0578f3
Ben Staton <bstaton@critfc.org>
2019-06-28 13:46:46 -0700

    update beta_mm documentation

--- R/beta_mm.R
+++ R/beta_mm.R
@@ -1,17 +1,19 @@
 #'Obtain the shape parameters of a beta distribution
 #'
-#'Use the method of moments to obtain the \code{shape1} and \code{shape2}
+#'Use the method of moments to obtain the \code{shape1} (alpha) and \code{shape2} (beta)
 #'parameters when provided a vector assumed iid beta random variables.
 #'
 #'@param p a vector of iid beta random variables, for which you want
 #'  to obtain the parameters of the beta distribution from which they
 #'  are drawn
+#'@param na.rm logical. Do you wish to remove NA elements before calculating mean and variance?
+#'  Defaults to \code{FALSE}.
 #'@export
 
-beta_mm = function(p) {
+beta_mm = function(p, na.rm = F) {
   # get estimates of the moments
-  m = mean(p)
-  v = var(p)
+  m = mean(p, na.rm = na.rm)
+  v = var(p, na.rm = na.rm)
 
   # get estimates of the shape parameters
   shape1 = m * ((m * (1 - m))/v - 1)
