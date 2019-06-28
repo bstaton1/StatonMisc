72313bcaf898df990cba8d3f438cd4829c8bf73e
Ben Staton <bstaton@critfc.org>
2019-06-28 14:23:08 -0700

    update documentation for sig2cv

--- R/sig2cv.R
+++ R/sig2cv.R
@@ -1,12 +1,12 @@
 #' Convert Lognormal SD to CV
 #'
-#' This function is used to convert the lognormal standard deviation
+#' Used to convert the lognormal standard deviation
 #' of a random variable to the coefficient of variation
 #' using a simple transformation.
 #'
 #' @param cv A numeric vector, possibly length > 1, containing the lognormal SD you wish to convert.
 #'
-#' @return A numeric vector with the same length as \code{cv},
+#' @return A numeric vector with the same length as \code{sig},
 #'   the coefficient of variation.
 #'
 #' @seealso \code{\link{cv2sig}}
