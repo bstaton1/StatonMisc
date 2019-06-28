193ff8c0a63b2738f167f473af01ad5511a1871c
Ben Staton <bstaton@critfc.org>
2019-06-28 14:21:06 -0700

    update is_leap_year and export it

--- R/is_leap_year.R
+++ R/is_leap_year.R
@@ -1,25 +1,37 @@
 #' Determine if a year is a leap year
 #'
-#' This function is used to determine if each element
-#' in a vector of years is a leap year. It is a hidden function
-#' in this package.
+#' Used to determine if each element in a vector of years is a leap year.
 #'
 #' @param date a numeric vector of length >= 1 containing the years to test
 #'
 #' @return a logical vector indicating whether each year is a leap year (\code{TRUE} if so, \code{FALSE} if not).
 #'
+#' @details a year is considered a leap year if it is divisible by 4 and not divisible by 100.
+#'   The exception is years that are divisible by 400 are also considered leap years.
+#'
 #' @examples
 #' is_leap_year(2016:2020)
+#'
+#' @export
 
 is_leap_year = function(year) {
-  # function to determine if x is a whole number
-  is_whole_number = function(x) {
-    x == round(x)
+  # determine if the year is divisible by each number
+  d4 = year %% 4 == 0
+  d100 = year %% 100 == 0
+  d400 = year %% 400 == 0
+
+  out = logical(length(year))
+  for (i in 1:length(year)) {
+    if (d4[i]) {
+      if (d100[i] & !d400[i]) {
+        out[i] = FALSE
+      } else {
+        out[i] = TRUE
+      }
+    } else {
+      out[i] = FALSE
+    }
   }
 
-  yr_type = ifelse(!is_whole_number(year/4), "common",
-                   ifelse(!is_whole_number(year/100), "leap",
-                          ifelse(!is_whole_number(year/400), "common", "leap")))
-
-  yr_type == "leap"
+  out
 }
