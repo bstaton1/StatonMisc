32ffea6b9963a0b17348ce665f2c90db912d0b7f
Ben Staton <bstaton@critfc.org>
2019-10-28 09:38:31 -0700

    create the progress_updater function and docs
    I am increasingly using this code to print the status of long running calculations. This just wraps it a nice function.
    
    This change constitutes version 0.1.3

---
+++ R/progress_updater.R
@@ -0,0 +1,26 @@
+#' Print a progress indicator to console
+#'
+#' Include this function in a loop or other calculation that iterates over elements
+#' in which the total number of elements is known to obtain an indication of what percentage
+#' of the calculation is completed
+#'
+#' @param i numeric vector of length == 1. The current iteration value
+#' @param n numeric vector of length == 1. The number of iterations to calculate
+#' @param grp character or numeric vector of length == 1. Optional additional identifier to be placed
+#'   in parentheses following the printed percent. Defaults to \code{NULL}, in which case no additional
+#'   group identifier is printed
+#' @param rnd numeric vector of length == 1. Controls the number of decimal values in the printed percent.
+#'   Defaults to \code{rnd = 0}, in which case only the integer value percents will be printed.
+#'   Set to \code{rnd = 1} to obtain tenths of a percent, \code{rnd = 2} to obtain hundredths, etc.
+#' @export
+#' @examples
+#'  junk = sapply(1:15, function(i) {progress_updater(i, 15, rnd = 1); Sys.sleep(0.2)})
+
+progress_updater = function(i, n, grp = NULL, rnd = 0) {
+  # clear the last printed line
+  cat("\r                                              ")
+  # print the progress for this line
+  cat("\r", round(i/n, rnd+2) * 100, "%", ifelse(!is.null(grp), paste0(" (", grp, ")"), ""), sep = "")
+  # print a new line if this is the last i
+  if (i == n) cat("\n")
+}
