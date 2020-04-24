909391f005dd1fce2a978559199654b1c34c9d68
Ben Staton <statonbe@gmail.com>
2020-04-24 15:24:28 -0700

    Update file_device to include eps output
    Also improve the function body: no longer need eval(parse(text = ...)), now using a do.call()

--- R/file_device.R
+++ R/file_device.R
@@ -1,24 +1,24 @@
-#' Create a graphics device file based on file extension
+#' Create a graphics device file based on supplied file extension
 #'
-#' Wouldn't it be nice to switch between (e.g.,) pdf and jpg
-#' files by simply changing the file extension?
+#' General purpose graphics devices saved to files, where the file type is determined by the file name
 #'
-#' @param file the name of the output file. Can be a path,
+#' @param filename the name of the output file. Can be a path
 #'   but must contain a file extension at the end (e.g., \code{"path/to/dir/file.png"})
-#' @param height the height of the device, in inches. Defaults to 5
 #' @param width the width of the device, in inches. Defaults to 5
-#' @param res the resolution of the device, in pixels per inch. Defaults to 600
-#' @details Accepted file extentions include:
+#' @param height the height of the device, in inches. Defaults to 5
+#' @param res the resolution of the device, in pixels per inch. Defaults to 600. Has no effect if the extension is \code{".pdf"} or \code{".eps"}
+#' @param ... optional arguments passed to the graphics device generating function (e.g., \code{\link[grDevices]{png}})
+#' @details Accepted file extensions include:
 #' \itemize{
-#'   \item \code{".pdf"}
-#'   \item \code{".png"}
-#'   \item \code{".bmp"}
-#'   \item \code{".jpg"} or \code{".jpeg"}
-#'   \item \code{".tif"} or \code{".tiff"}
+#'   \item \code{".pdf"} (calls \code{\link[grDevices]{pdf}})
+#'   \item \code{".png"} (calls \code{\link[grDevices]{png}})
+#'   \item \code{".bmp"} (calls \code{\link[grDevices]{bmp}})
+#'   \item \code{".jpg"} (or \code{".jpeg"}, both call \code{\link[grDevices]{jpeg}}))
+#'   \item \code{".tif"} (or \code{".tiff"}, both call \code{\link[grDevices]{tiff}}))
+#'   \item \code{".eps"} (calls \code{\link[grDevices]{cairo_ps}})
 #' }
+#' @note A call to \code{\link[grDevices]{dev.off}} is still required after the plotting code (see examples).
 #' @export
-#' @seealso \code{\link[grDevices]{pdf}}, \code{\link[grDevices]{jpeg}},
-#'   \code{\link[grDevices]{png}}, \code{\link[grDevices]{tiff}}, \code{\link[grDevices]{bmp}}
 #' @examples
 #' \dontrun{
 #' # use a png device
@@ -33,50 +33,52 @@
 #' dev.off()
 #' }
 
-file_device = function(file, width = 5, height = 5, res = 600) {
+file_device = function(filename, width = 5, height = 5, res = 600, ...) {
   # extract the file extension
   file_type = stringr::str_remove(
-    stringr::str_extract(basename(file), "\\.([[:alnum:]]+)$"),
+    stringr::str_extract(basename(filename), "\\.([[:alnum:]]+)$"),
     "\\."
   )
 
   # stop if no extension found
-  if (is.na(file_type)) {
-    stop ("No file extension found in the supplied file")
-  }
+  if (is.na(file_type)) stop ("No file extension found in the supplied file")
 
   # check to ensure the file extention will match up with an accepted plotting function
-  accepted_formats = c("jpeg", "jpg", "png", "pdf", "tiff", "tif", "bmp")
+  accepted_formats = c("jpeg", "jpg", "png", "pdf", "tiff", "tif", "bmp", "eps")
   if (file_type %!in% accepted_formats) {
     stop ("\n  File extension '", file_type, "' does not correspond to an accepted graphics device.\n  Valid file extentions include:",
-          StatonMisc::list_out(accepted_formats, final = "or", wrap = "'"))
+          list_out(accepted_formats, final = "or", wrap = "'"))
   }
 
   # obtain the name of the base graphics function to use
-  # if jpg or tif were the extensions, change these to the function name
+  # if jpg, tif, or eps were the extensions, change these to the appropriate function name
   fun_name = switch(
     file_type,
     jpg = "jpeg",
     tif = "tiff",
+    eps = "cairo_ps",
     file_type
   )
 
-  # if using pdf device, change res to be 1 (pdf devices don't use resolution arguments)
-  if (fun_name == "pdf") res = 1
+  # which devices take a res argument?
+  res_devs = accepted_formats[accepted_formats %!in% c("pdf", "eps")]
 
-  # build the first part of the function text
-  first = paste0(fun_name, "('", file, "'")
+  # if not using a res device, change res to be 1
+  if (fun_name %!in% res_devs) res = 1
 
-  # build the dimension arguments
-  h_arg = paste0("height = ", height, " * res")
-  w_arg = paste0("width = ", width, " * res")
+  # build the args list
+  args_list = list(
+    filename = filename,
+    height = height * res,
+    width = width * res
+  )
 
-  # build the last part of the function text (include res if not pdf)
-  if (fun_name != "pdf") last = paste0("res = ", res, ")") else last = ")"
+  # add res to args if necessary
+  if (file_type %in% res_devs) args_list = append(args_list, list(res = res))
 
-  # build the text to parse
-  parse_me = paste(first, h_arg, w_arg, last, sep = ", ")
+  # add dot arguments if necessary
+  if (...length() > 0) args_list = append(args_list, list(...))
 
-  # evaluate the parsed text
-  eval(parse(text = parse_me))
+  # call the appropriate graphics device function with the appropriate arguments
+  do.call(fun_name, args_list)
 }
